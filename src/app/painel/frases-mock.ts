d908a462786bdafb222b66422b1811f8b2e8b6ee
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-03-07 00:35:44 -0300

    mock de objetos frases

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from './../shared/frase.model';
+
+const FRASES: Frase[] = [
+   { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+   { fraseEng: 'I Watch tv', frasePtBr: 'Eu assisto tv' },
+   { fraseEng: 'I dont like yellow color', frasePtBr: 'Eu nao gosto da cor amarela' },
+   { fraseEng: 'How are u?', frasePtBr: 'Como vai você?' },
+]
